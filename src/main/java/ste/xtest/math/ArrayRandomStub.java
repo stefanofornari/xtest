b4907dba26483238272633fb43f43678138e869d
Stefano Fornari <stefano.fornari@gmail.com>
2015-11-08 15:42:32 +0100

    initial revision - stub for java.util.Random

---
+++ src/main/java/ste/xtest/math/ArrayRandomStub.java
@@ -0,0 +1,165 @@
+/*
+ * xTest
+ * Copyright (C) 2015 Stefano Fornari
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by
+ * the Free Software Foundation with the addition of the following permission
+ * added to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED
+ * WORK IN WHICH THE COPYRIGHT IS OWNED BY Stefano Fornari, Stefano Fornari
+ * DISCLAIMS THE WARRANTY OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, see http://www.gnu.org/licenses or write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301 USA.
+ */
+package ste.xtest.math;
+
+import java.util.Random;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+
+/**
+ *
+ */
+public class ArrayRandomStub extends Random {
+    private final int[] numbers;
+    private int index;
+    
+    public ArrayRandomStub(final int[] numbers) {
+        if (numbers == null) {
+            throw new IllegalArgumentException("numbers can not be null");
+        }
+        if (numbers.length == 0) {
+            throw new IllegalArgumentException("numbers can not be of length zero");
+        }
+        
+        this.numbers = numbers;
+        this.index = 0;
+    }
+    
+    public int[] getNumbers() {
+        return numbers;
+    }
+    
+    public void reset() {
+        this.index = 0;
+    }
+    
+    // --------------------------------------------------------------- overrides
+    
+    @Override
+    protected int next(int bits) {
+        return numbers[(index++ % numbers.length)];
+    }
+    
+    @Override
+    public boolean nextBoolean() {
+        throw new RuntimeException("not implemented");
+    }
+    
+    @Override
+    public void nextBytes(byte[] numbers) {
+        for (int i=0; i<numbers.length; ++i) {
+            numbers[i] = (byte)next(32);
+        }
+    }
+    
+    @Override
+    public double nextDouble() {
+        throw new RuntimeException("not implemented");
+    }
+    
+    @Override
+    public float nextFloat() {
+        throw new RuntimeException("not implemented");
+    }
+    
+    @Override
+    public double nextGaussian() {
+        throw new RuntimeException("not implemented");
+    }
+    
+    @Override
+    public int nextInt() {
+        return next(32);
+    }
+    
+    @Override
+    public int nextInt(int bound) {
+        throw new RuntimeException("not implemented");
+    }
+    
+    @Override
+    public long nextLong() {
+        throw new RuntimeException("not implemented");
+    }
+    
+    @Override
+    public DoubleStream doubles() {
+        throw new RuntimeException("not implemented");
+    }
+    
+    @Override
+    public DoubleStream doubles(double randomNumberOrigin, double randomNumberBound) {
+        throw new RuntimeException("not implemented");
+    }
+    
+    @Override
+    public DoubleStream doubles(long streamSize) {
+        throw new RuntimeException("not implemented");
+    }
+    
+    @Override
+    public DoubleStream doubles(long streamSize, double randomNumberOrigin, double randomNumberBound) {
+        throw new RuntimeException("not implemented");
+    }
+    
+    @Override
+    public IntStream ints() {
+        throw new RuntimeException("not implemented");
+    }
+    
+    @Override
+    public IntStream ints(int randomNumberOrigin, int randomNumberBound) {
+        throw new RuntimeException("not implemented");
+    }
+    
+    @Override
+    public IntStream ints(long streamSize) {
+        throw new RuntimeException("not implemented");
+    }
+    
+    @Override
+    public IntStream ints(long streamSize, int randomNumberOrigin, int randomNumberBound) {
+        throw new RuntimeException("not implemented");
+    }
+    
+    @Override
+    public LongStream longs() {
+        throw new RuntimeException("not implemented");
+    }
+    
+    @Override
+    public LongStream longs(long randomNumberOrigin, long randomNumberBound) {
+        throw new RuntimeException("not implemented");
+    }
+    
+    @Override
+    public LongStream longs(long streamSize) {
+        throw new RuntimeException("not implemented");
+    }
+    
+    @Override
+    public LongStream longs(long streamSize, long randomNumberOrigin, long randomNumberBound) {
+        throw new RuntimeException("not implemented");
+    }
+    
+}
---
+++ src/test/java/ste/xtest/math/BugFreeArrayRandomStub.java
@@ -0,0 +1,125 @@
+/*
+ * xTest
+ * Copyright (C) 2015 Stefano Fornari
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by
+ * the Free Software Foundation with the addition of the following permission
+ * added to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED
+ * WORK IN WHICH THE COPYRIGHT IS OWNED BY Stefano Fornari, Stefano Fornari
+ * DISCLAIMS THE WARRANTY OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, see http://www.gnu.org/licenses or write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301 USA.
+ */
+package ste.xtest.math;
+
+import static org.assertj.core.api.BDDAssertions.then;
+import static org.junit.Assert.fail;
+import org.junit.Test;
+
+/**
+ * @TODO: invalid arguments check for constructor
+ * 
+ * @author ste
+ */
+public class BugFreeArrayRandomStub {
+    @Test
+    public void constructor_accepts_a_byte_array() {
+        int[] numbers = new int[10];
+        
+        ArrayRandomStub bar = new ArrayRandomStub(numbers);
+        then(bar.getNumbers()).isEqualTo(numbers);
+        
+        numbers = new int[10]; // same content different reference
+        bar = new ArrayRandomStub(numbers);
+        then(bar.getNumbers()).isEqualTo(numbers);
+        
+        numbers = new int[] {10, 54, 590};
+        bar = new ArrayRandomStub(numbers);
+        then(bar.getNumbers()).isEqualTo(numbers);
+    }
+    
+    @Test
+    public void nextInt_returns_the_given_sequence() {
+        int[] numbers = new int[] {10, 54, 590};
+        
+        ArrayRandomStub bar = new ArrayRandomStub(numbers);
+        
+        for(int i=0; i<numbers.length; ++i) {
+            then(bar.nextInt()).isEqualTo(numbers[i]);
+        }
+    }
+    
+    @Test
+    public void nextBytes_returns_the_given_sequence_with_overflow() {
+        int[] numbers = new int[] {10, 54, 123};
+        
+        ArrayRandomStub bar = new ArrayRandomStub(numbers);
+        
+        byte[] randoms = new byte[2];
+        bar.nextBytes(randoms);
+        then(randoms).isEqualTo(new byte[] {(byte)numbers[0], (byte)numbers[1]});
+        
+        randoms = new byte[5];
+        bar.nextBytes(randoms);
+        then(randoms).isEqualTo(
+            new byte[] {
+                (byte)numbers[2], (byte)numbers[0],
+                (byte)numbers[1], (byte)numbers[2],
+                (byte)numbers[0]
+            }
+        );
+    }
+    
+    @Test 
+    public void next_starts_from_zero_on_overflow() {
+        int[] numbers = new int[] {10, 54, 590};
+        
+        ArrayRandomStub bar = new ArrayRandomStub(numbers);
+        
+        for(int i=0; i<numbers.length; ++i) {
+            then(bar.nextInt()).isEqualTo(numbers[i]);
+        }
+        for(int i=0; i<numbers.length; ++i) {
+            then(bar.nextInt()).isEqualTo(numbers[i]);
+        }
+    }
+    
+    @Test
+    public void byte_array_can_not_be_null_or_zero_length() {
+        try {
+            new ArrayRandomStub(null);
+            fail("missing arguments validity check");
+        } catch (IllegalArgumentException x) {
+            then(x).hasMessage("numbers can not be null");
+        }
+        
+        try {
+            new ArrayRandomStub(new int[0]);
+            fail("missing arguments validity check");
+        } catch (IllegalArgumentException x) {
+            then(x).hasMessage("numbers can not be of length zero");
+        }
+    }
+    
+    @Test
+    public void reset_restarts_the_sequence() {
+        ArrayRandomStub bar = new ArrayRandomStub(new int[] {10, 54, 123, 39, 209, 0});
+        bar.nextBytes(new byte[3]); bar.reset();
+        byte[] numbers = new byte[3]; bar.nextBytes(numbers);
+        then(numbers).isEqualTo(new byte[] {10, 54, 123});
+        
+        bar.nextInt(); bar.reset();
+        then(numbers).isEqualTo(new byte[] {10, 54, 123});
+    }
+    
+    
+}
